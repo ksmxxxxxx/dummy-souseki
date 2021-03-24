65bfbd19a0e607907f083c2198e1ca4333fcb9cf
ksmxxxxxx <kankichi69@gmail.com>
2021-03-24 15:10:06 +0900

    :bug: Resolve load path error when run with command

--- src/bin/getVersion.js
+++ src/bin/getVersion.js
@@ -1,5 +1,5 @@
 import fs from 'fs'
-const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
+const packageJson = JSON.parse(fs.readFileSync(new URL('../../package.json', import.meta.url), 'utf-8'))
 
 const getVersion = () => { return packageJson.version }
 
